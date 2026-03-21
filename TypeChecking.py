22531bc7a9ee9b69669a7d3d025ec549dcb53220
SoumiliSamanta <soumili.samanta18@gmail.com>
2026-03-22 01:32:22 +0530

    added new file

---
+++ TypeChecking.py
@@ -0,0 +1,8 @@
+a = input("value of a:")
+type(a)# a = 10 <integer>
+b = input("value of b:")
+type(b)# b = 2.5 <float>
+c = input("value of c:")
+type(c)#c = "hello" <string>
+d = input("value of d:")
+type(d)#d = True <boolean>
