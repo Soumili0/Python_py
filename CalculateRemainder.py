e5aa56945ffbd8a89463915a6bbb2270b017acf2
SoumiliSamanta <soumili.samanta18@gmail.com>
2026-03-22 01:26:57 +0530

    python practice codes

---
+++ CalculateRemainder.py
@@ -0,0 +1,4 @@
+a = input("enter first number: ")
+z = input("enter second number: ")
+r = int(a) % int(z)
+print("remainder of the two number is:",r)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+a = input("enter 1st value: ")
+b = input("enter 2nd value: ")
+c = print("sum of the two number is:",(a+b))
