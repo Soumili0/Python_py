a14c32df3ecf7f134c6e1081fb2ab17124a0d119
Soumili Samanta <soumili.samanta18@gmail.com>
2026-03-25 23:21:17 +0530

    Update SpamComment.py

--- SpamComment.py
+++ SpamComment.py
@@ -1,6 +1,6 @@
-#A spam comment is defined as a text containing following keywords: 
-#“Make a lot of money”, “buy now”, “subscribe this”, “click this”. Write a program 
-#to detect these spams.
+'''A spam comment is defined as a text containing following keywords: 
+“Make a lot of money”, “buy now”, “subscribe this”, “click this”. Write a program 
+to detect these spams.'''
 # Define spam keywords
 spam_keywords = ["Make a lot of money", "buy now", "subscribe this", "click this"]
 # Take input from user
