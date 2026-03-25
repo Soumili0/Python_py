9ac3f076b81a624a6d6f08f53dcfec9bf0a3cc24
Soumili Samanta <soumili.samanta18@gmail.com>
2026-03-25 23:12:59 +0530

    Update AcceptMarks.py

--- AcceptMarks.py
+++ AcceptMarks.py
@@ -4,10 +4,10 @@ for i in range(6):
     marks.append(m)
     marks.sort()
 print("The marks you entered in sorted order are:", marks)
-#Enter the marks of subject 1: 78
-#Enter the marks of subject 2: 96
-#Enter the marks of subject 3: 90
-#Enter the marks of subject 4: 56
-#Enter the marks of subject 5: 91
-#Enter the marks of subject 6: 83
-#The marks you entered in sorted order are: [56, 78, 83, 90, 91, 96]
+'''Enter the marks of subject 1: 78
+Enter the marks of subject 2: 96
+Enter the marks of subject 3: 90
+Enter the marks of subject 4: 56
+Enter the marks of subject 5: 91
+Enter the marks of subject 6: 83
+The marks you entered in sorted order are: [56, 78, 83, 90, 91, 96]'''
