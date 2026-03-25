71b3c87b5af7eebb9ec0dc5bb330e8f20ad04c68
Soumili Samanta <soumili.samanta18@gmail.com>
2026-03-25 23:20:19 +0530

    Update PassOrFail.py

--- PassOrFail.py
+++ PassOrFail.py
@@ -1,7 +1,7 @@
-#Write a program to find out whether a student has passed or failed if it requires a 
-#total of 40% and at least 33% in each subject to pass. Assume 3 subjects and 
-#take marks as an input from the user.
-# Input marks for 3 subjects
+'''Write a program to find out whether a student has passed or failed if it requires a 
+total of 40% and at least 33% in each subject to pass. Assume 3 subjects and 
+take marks as an input from the user.'''
+#Input marks for 3 subjects
 marks = []
 for i in range(3):
     mark = int(input(f"Enter marks for subject {i+1}: "))
