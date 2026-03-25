f977e1c8053a62c3b2c7ef69c0d05ded100fbbe4
Soumili Samanta <soumili.samanta18@gmail.com>
2026-03-25 23:24:39 +0530

    Update sumLists.py

--- sumLists.py
+++ sumLists.py
@@ -4,8 +4,8 @@ for i in range(4):
     numbers.append(n)
     total = sum(numbers)
 print("The sum of the numbers is:", total)
-#Enter number 1: 56
-#Enter number 2: 90
-#Enter number 3: 12
-#Enter number 4: 34
-#The sum of the numbers is: 192
+'''Enter number 1: 56
+Enter number 2: 90
+Enter number 3: 12
+Enter number 4: 34
+The sum of the numbers is: 192'''
