d51902939cc8de90870c8556df95c0efd4d2de63
Soumili Samanta <soumili.samanta18@gmail.com>
2026-03-25 23:17:15 +0530

    Update Dic&Set3.py

--- Dic&Set3.py
+++ Dic&Set3.py
@@ -1,5 +1,6 @@
-#Create an empty dictionary. Allow 4 friends to enter their favorite language as 
-#value and use key as their names. Assume that the names are unique. 
+'''Create an empty dictionary. Allow 4 friends to enter their
+favorite language as value and use key as their
+names. Assume that the names are unique. '''
 friends = {}
 
 for i in range(4):
@@ -12,17 +13,17 @@ print("\nFavorite languages:", friends)
 friends = {'A': 'Python'}
 friends['A'] = 'Java'
 print(friends)
-#Output: {'A': 'Java'}
-#In the above code, since the key 'A' is used twice, 
-# the second assignment will overwrite the first one. 
-# Therefore, the final output will be {'A': 'Java'}, 
-# and the original value 'Python' will be lost. 
-
+'''Output: {'A': 'Java'}
+In the above code, since the key 'A' is used twice, 
+the second assignment will overwrite the first one. 
+Therefore, the final output will be {'A': 'Java'}, 
+and the original value 'Python' will be lost. 
+'''
 
 #If languages of two friends are same; what will happen to the program ?
 friends = {'A': 'Python', 'B': 'Python'}
 print(friends)
-#Output: {'A': 'Python', 'B': 'Python'}
-#In this case, since the keys 'A' and 'B' are different,
-# both friends can have the same favorite language 'Python' 
-# without any issues.
+'''Output: {'A': 'Python', 'B': 'Python'}
+In this case, since the keys 'A' and 'B' are different,
+both friends can have the same favorite language 'Python' 
+without any issues.'''
