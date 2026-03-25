454d3fa955590bbcf6f92338dfbfab30c30b7202
Soumili Samanta <soumili.samanta18@gmail.com>
2026-03-25 23:15:21 +0530

    Fix indentation and formatting issues in Hindi dictionary

--- Dic&Set1.py
+++ Dic&Set1.py
@@ -1,5 +1,5 @@
-# Write a program to create a dictionary of Hindi words with values as their English
-# translation. Provide user with an option to look it up!
+''' Write a program to create a dictionary of Hindi words with values as their English
+translation. Provide user with an option to look it up!'''
 
 # Create dictionary
 hindi_dict = {
