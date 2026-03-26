bd11a2d75c5b92d06a6e442a7cea4fefc4b55746
Soumili Samanta <soumili.samanta18@gmail.com>
2026-03-26 22:58:58 +0530

    Add files via upload

---
+++ Greatest.py
@@ -0,0 +1,16 @@
+'''
+Write a program using functions
+to find greatest of three numbers. 
+'''
+def greatest_of_three(a, b, c):
+    if a >= b and a >= c:
+        return a
+    elif b >= a and b >= c:
+        return b
+    else:
+        return c
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+result = greatest_of_three(num1, num2, num3)
+print("The greatest of the three numbers is:", result)
